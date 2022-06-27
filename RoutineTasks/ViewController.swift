88e575840de041773fae51a9c329e77bb8419f19
Oksenoyt <a.oksenoyt@gmail.com>
2022-06-27 15:11:03 +0300

    added header panel

--- RoutineTasks/ViewController.swift
+++ RoutineTasks/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var settingsButton: UIButton!
+    @IBOutlet weak var addNewItemButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        
+        addNewItemButton.tintColor = .white
     }
 
 
