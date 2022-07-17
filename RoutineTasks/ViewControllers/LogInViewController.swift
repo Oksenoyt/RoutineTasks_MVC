2d56b376ee93fb9d8a37e25067ce549b9d2a85e7
Oksenoyt <a.oksenoyt@gmail.com>
2022-07-17 08:16:59 +0300

    Added model and LogIn Screen

---
+++ RoutineTasks/ViewControllers/LogInViewController.swift
@@ -0,0 +1,39 @@
+//
+//  LogInViewController.swift
+//  RoutineTasks
+//
+//  Created by Elena Kholodilina on 17.07.2022.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController {
+
+    @IBOutlet var loginTextField: UITextField!
+    @IBOutlet var passwordTextField: UITextField!
+    
+    let user = User.getUser()
+    
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//    }
+    
+override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        view.endEditing(true)
+    }
+    
+    
+    
+}
+
+extension LogInViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "Ok", style: .default)
+        
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
