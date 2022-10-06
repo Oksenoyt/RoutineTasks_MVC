869f90d0998bacdfe6a9e114e7da88373f971adf
Oksenoyt <a.oksenoyt@gmail.com>
2022-10-06 17:49:19 +0400

    fix

--- RoutineTasks/ViewControllers/NewItemViewController.swift
+++ RoutineTasks/ViewControllers/NewItemViewController.swift
@@ -18,59 +18,75 @@ class NewItemViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-//        nameTextField.layer.borderWidth = 2
-//        nameTextField.layer.borderColor = UIColor.black.cgColor
-//        nameTextField.layer.cornerRadius = 25
-//        nameTextField.layer.masksToBounds = true
-
-    }
-    override func viewWillLayoutSubviews() {
-        createButton.layer.cornerRadius = 15
         view.layer.cornerRadius = 30
-//        nameTextField.borderStyle = .roundedRect
-        nameTextField.layer.cornerRadius = 30
-        nameTextField.clearButtonMode = .whileEditing
-        
+        createButton.layer.cornerRadius = 15
+        setTextField()
         for itemColorView in itemColorUIStackView {
             itemColorView.layer.cornerRadius = 15
         }
+        
     }
-
+    //    override func viewWillLayoutSubviews() {
+    //        for itemColorView in itemColorUIStackView {
+    //            itemColorView.layer.cornerRadius = 15
+    //        }
+    //    }
+    
     @IBAction func createButton(_ sender: UIButton) {
-        var name = "Уборка домааа1" //let
+        guard let name = nameTextField.text, !name.isEmpty else {
+            // сделать алерт
+            return }
+        
+//        var name = "Уборка домааа1" //let
         let color = "#c49dcc"
         let currentData = date.getDateString(dayBefore: 0)
-        //сделать проверку на одинаковые названия
         
-        StorageManager.shared.create(taskName: name, color: color, startDate: date.getDateString(dayBefore: 0)) { task in
+        StorageManager.shared.create(taskName: name, color: color, startDate: currentData) { task in
             print(1)
         }
-        name = "Уборка домааа1"
-        StorageManager.shared.create(taskName: name, color: color, startDate: date.getDateString(dayBefore: 1)) { task in
-            print(2)
-        }
-        name = "Уборка домааа1"
         
-        StorageManager.shared.create(taskName: name, color: color, startDate: date.getDateString(dayBefore: 2)) { task in
-            print(3)
-        }
-        name = "qweqwe3"
         
-        StorageManager.shared.create(taskName: name, color: color, startDate: date.getDateString(dayBefore: 3)) { task in
-            print(4)
-        }
-        
+        //        var name = "Уборка домааа1" //let
+        //        let color = "#c49dcc"
+        //        let currentData = date.getDateString(dayBefore: 0)
+        //        //сделать проверку на одинаковые названия
+        //
+        //        StorageManager.shared.create(taskName: name, color: color, startDate: date.getDateString(dayBefore: 0)) { task in
+        //            print(1)
+        //        }
+        //        name = "Уборка домааа1"
+        //        StorageManager.shared.create(taskName: name, color: color, startDate: date.getDateString(dayBefore: 1)) { task in
+        //            print(2)
+        //        }
+        //        name = "Уборка домааа1"
+        //
+        //        StorageManager.shared.create(taskName: name, color: color, startDate: date.getDateString(dayBefore: 2)) { task in
+        //            print(3)
+        //        }
+        //        name = "qweqwe3"
+        //
+        //        StorageManager.shared.create(taskName: name, color: color, startDate: date.getDateString(dayBefore: 3)) { task in
+        //            print(4)
+        //        }
     }
     
+    private func setTextField() {
+        nameTextField.layer.masksToBounds = true
+        nameTextField.layer.cornerRadius = 30
+        nameTextField.clearButtonMode = .whileEditing
+        nameTextField.becomeFirstResponder()
+        nameTextField.returnKeyType = UIReturnKeyType.done ///????
+        //       nameTextField.delegate = self
+    }
     
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
